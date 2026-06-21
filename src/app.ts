c429ceb8bcfb721b3783bc39c213eaa073a6aaf8
Ayas Ibrahim <aayasabraham@gmail.com>
2026-06-22 00:49:19 +0600

    feat: add cookie-parser and update package-lock.json

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import express,{ Application, Request, Response } from "express";
+import config from "./config";
+import cors from "cors"
+import cookieParse from "cookie-parser"
 
 const app:Application=express()
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParse())
 app.get("/",(req:Request,res:Response)=>{
     res.send("hello Word")
 })
